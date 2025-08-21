96aa31bd6da0430ebb6d81603ad83cd7c283e79d
henry3977 <38033718+henry3977@users.noreply.github.com>
2025-08-21 20:49:08 +0900

    hotfix

--- resources/views/livewire/parent/home.blade.php
+++ resources/views/livewire/parent/home.blade.php
@@ -39,7 +39,7 @@
 
         <!-- Learning Status -->
         <div class="bg-fuchsia-50 rounded-lg shadow-sm p-3 sm:p-4 mb-4">
-            @if ($weeklyReport['test_report'] || $weeklyReport['homework_report'] || $weeklyReport['comment_report'])
+            @if ($weeklyReport && ($weeklyReport['test_report'] || $weeklyReport['homework_report'] || $weeklyReport['comment_report']))
                 <div class="flex justify-between items-center mb-4">
                     <h3 class="text-lg font-bold text-stone-900">학습현황</h3>
                     <div class="flex items-center space-x-2">
