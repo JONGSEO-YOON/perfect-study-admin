064763d35270c3f308d4a1f105977cc7abc777fb
henry3977 <38033718+henry3977@users.noreply.github.com>
2025-09-22 16:51:00 +0900

    푸시 data 만

--- app/Services/FcmService.php
+++ app/Services/FcmService.php
@@ -57,20 +57,20 @@ class FcmService
                     //     'title' => $title,
                     //     'body' => $body
                     // ],
-                    // 'data' => array_map('strval', $data),
-                    'webpush' => [
-                        'headers' => [
-                            'Urgency' => 'high'
-                        ],
-                        'notification' => [
-                            'title' => $title,
-                            'body' => $body,
-                            'icon' => '/icon-parent-192x192.png',
-                            'badge' => '/icon-parent-192x192.png',
-                            'tag' => 'perfect-study-notification',
-                            'requireInteraction' => true
-                        ]
-                    ]
+                    'data' => array_map('strval', $data),
+                    // 'webpush' => [
+                    //     'headers' => [
+                    //         'Urgency' => 'high'
+                    //     ],
+                    //     'notification' => [
+                    //         'title' => $title,
+                    //         'body' => $body,
+                    //         'icon' => '/icon-parent-192x192.png',
+                    //         'badge' => '/icon-parent-192x192.png',
+                    //         'tag' => 'perfect-study-notification',
+                    //         'requireInteraction' => true
+                    //     ]
+                    // ]
                 ]
             ];
             Log::info('FCM 메시지 data: ' . json_encode($data));
