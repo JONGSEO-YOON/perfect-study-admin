96c9ec5c7f5cca81d5e06112641375e68f3c7ac4
Ubuntu <ubuntu@ip-172-31-11-190.ap-northeast-2.compute.internal>
2026-03-28 00:58:23 +0000

    학생 이력 타임라인 아이콘 렌더링 수정

--- resources/views/filament/components/modals/student-history.blade.php
+++ resources/views/filament/components/modals/student-history.blade.php
@@ -17,60 +17,27 @@
 
             <div class="space-y-4">
                 @foreach($timeline as $event)
+                    @php
+                        $colorClasses = match($event['color']) {
+                            'blue' => ['bg' => 'bg-blue-100 text-blue-600', 'text' => 'text-blue-700'],
+                            'green' => ['bg' => 'bg-green-100 text-green-600', 'text' => 'text-green-700'],
+                            'emerald' => ['bg' => 'bg-emerald-100 text-emerald-600', 'text' => 'text-emerald-700'],
+                            'red' => ['bg' => 'bg-red-100 text-red-600', 'text' => 'text-red-700'],
+                            'orange' => ['bg' => 'bg-orange-100 text-orange-600', 'text' => 'text-orange-700'],
+                            'indigo' => ['bg' => 'bg-indigo-100 text-indigo-600', 'text' => 'text-indigo-700'],
+                            default => ['bg' => 'bg-gray-100 text-gray-600', 'text' => 'text-gray-700'],
+                        };
+                    @endphp
                     <div class="relative flex items-start gap-4 pl-12">
                         {{-- 아이콘 원 --}}
-                        <div class="absolute left-2 w-7 h-7 rounded-full flex items-center justify-center
-                            @switch($event['color'])
-                                @case('blue') bg-blue-100 text-blue-600 @break
-                                @case('green') bg-green-100 text-green-600 @break
-                                @case('emerald') bg-emerald-100 text-emerald-600 @break
-                                @case('red') bg-red-100 text-red-600 @break
-                                @case('orange') bg-orange-100 text-orange-600 @break
-                                @case('indigo') bg-indigo-100 text-indigo-600 @break
-                                @default bg-gray-100 text-gray-600
-                            @endswitch
-                        ">
-                            @switch($event['icon'])
-                                @case('user-plus')
-                                    <x-heroicon-m-user-plus class="w-4 h-4" />
-                                    @break
-                                @case('user-minus')
-                                    <x-heroicon-m-user-minus class="w-4 h-4" />
-                                    @break
-                                @case('credit-card')
-                                    <x-heroicon-m-credit-card class="w-4 h-4" />
-                                    @break
-                                @case('x-circle')
-                                    <x-heroicon-m-x-circle class="w-4 h-4" />
-                                    @break
-                                @case('arrow-path')
-                                    <x-heroicon-m-arrow-path class="w-4 h-4" />
-                                    @break
-                                @case('check-circle')
-                                    <x-heroicon-m-check-circle class="w-4 h-4" />
-                                    @break
-                                @case('academic-cap')
-                                    <x-heroicon-m-academic-cap class="w-4 h-4" />
-                                    @break
-                                @default
-                                    <x-heroicon-m-clock class="w-4 h-4" />
-                            @endswitch
+                        <div class="absolute left-2 w-7 h-7 rounded-full flex items-center justify-center {{ $colorClasses['bg'] }}">
+                            @svg('heroicon-s-' . $event['icon'], 'w-4 h-4')
                         </div>
 
                         {{-- 내용 --}}
                         <div class="flex-1 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-3">
                             <div class="flex items-center justify-between">
-                                <span class="font-medium text-sm
-                                    @switch($event['color'])
-                                        @case('blue') text-blue-700 @break
-                                        @case('green') text-green-700 @break
-                                        @case('emerald') text-emerald-700 @break
-                                        @case('red') text-red-700 @break
-                                        @case('orange') text-orange-700 @break
-                                        @case('indigo') text-indigo-700 @break
-                                        @default text-gray-700
-                                    @endswitch
-                                ">
+                                <span class="font-medium text-sm {{ $colorClasses['text'] }}">
                                     {{ $event['title'] }}
                                 </span>
                                 <span class="text-xs text-gray-400">
